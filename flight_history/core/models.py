b747e7429bbdcd8140af3b5bc3e5bdeee8491af1
Raphael F <raphaelf.ti@gmail.com>
2019-04-22 18:09:21 -0300

    Flight
    Add data info file
    Create models of flights and airports
    Create simple import from csv to database
    Migrations

--- flight_history/core/models.py
+++ flight_history/core/models.py
@@ -1,17 +1,74 @@
 from django.db import models
 
 
+class Airport(models.Model):
+    icao = models.CharField(max_length=4, unique=True)
+    iata = models.CharField(max_length=3, unique=True)
+    name = models.TextField()
+    alias = models.TextField(null=True)
+    city = models.TextField()
+    state = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Aeropoto'
+
+
 class Flight(models.Model):
-    icao = models.CharField(max_length=50)
-    number = models.IntegerField()
-    di = models.CharField(max_length=1)
-    line = models.CharField(max_length=1)
-    departure_date = models.CharField(max_length=20, blank=True, null=True)
-    departure_date_real = models.CharField(
-        max_length=20, blank=True, null=True)
-    arrival_date = models.CharField(max_length=20, blank=True, null=True)
-    arrival_date_real = models.CharField(max_length=20, blank=True, null=True)
-    status = models.CharField(max_length=1)
-    arrival = models.CharField(max_length=10)
-    departure = models.CharField(max_length=10)
+
+    AUTHORIZATION_CODE = (
+        (0, 'Voo regular'),
+        (1, 'Voo extra com HOTRAN'),
+        (2, 'Voo extra sem HOTRAN'),
+        (3, 'Voo de retorno'),
+        (4, 'Inclusão de etapa em um voo previsto em HOTRAN'),
+        (5, 'Voo cargueiro'),
+        (6, 'Voo de serviço'),
+        (7, 'Voo de fretamento'),
+        (9, 'Voo charter'),
+        ('A', 'Voo de instrução'),
+        ('B', 'Voo de experiência')
+    )
+
+    TYPE_LINE = (
+        ('N', 'Nacional'),
+        ('I', 'Internacional'),
+        ('R', 'Regional'),
+        ('H', 'Sub-regional'),
+        ('E', 'Especial'),
+        ('C', 'Cargueiro'),
+        ('G', 'Cargueiro internacional'),
+        ('L', 'Rede postal')
+    )
+
+    FLIGHT_STATUS = (
+        ('C', 'Cancelado'),
+        ('R', 'Realizado'),
+    )
+
+    icao = models.CharField(max_length=3, verbose_name='Empresa Área')
+    number = models.IntegerField(verbose_name='Número do Vôo')
+    di = models.CharField(max_length=1, choices=AUTHORIZATION_CODE,
+                          default=0, verbose_name='Código de Autorização')
+    line = models.CharField(max_length=1, choices=TYPE_LINE,
+                            default='N', verbose_name='Código da Linha')
+
+    departure = models.ForeignKey(
+        'Airport', related_name='departure_airpot', on_delete=models.CASCADE, verbose_name='Aeroporto de Origem')
+    arrival = models.ForeignKey(
+        'Airport', related_name='arrival_airpot', on_delete=models.CASCADE, verbose_name='Aeroporto de Destino')
+
+    departure_date = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=True, null=True, verbose_name='Partida Prevista')
+    departure_date_real = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=True, null=True, verbose_name='Partida Real')
+    arrival_date = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=True, null=True, verbose_name='Chegada Prevista')
+    arrival_date_real = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=True, null=True, verbose_name='Chegada Real')
+
+    status = models.CharField(max_length=1, choices=FLIGHT_STATUS, default='R', verbose_name='Situação')
+
     code = models.CharField(max_length=2, blank=True, null=True)
--- flight_history/core/views.py
+++ flight_history/core/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Airport, Flight
+
+
+for year in range(2000, 2019):
+    for month in range(1, 13):
+
+        try:
+            file = '/projects/mindstorm/brazil_flight_history/data/{year}/VRA_{year}{month}.csv'.format(
+                year=year, month=month)
+
+            with open(file, encoding="ISO-8859-1") as f:
+                reader = csv.reader(f)
+                next(reader, None)  # skip header
+
+                for row in reader:
+                    row = next(reader, None)
+
+                    line = row[3]
+                    if line != 'N':
+                        continue
+
+                    flight_status = row[10]
+                    if flight_status == 'Realizado':
+                        flight_status = 'R'
+                    else:
+                        flight_status = 'C'
+
+                    code = row[11]
+                    if code == 'N/A':
+                        code = None
+
+                    try:
+                        airport_departure = Airport.objects.get(icao=row[4])
+                        airport_arrival = Airport.objects.get(icao=row[5])
+
+                        instance = Flight.objects.get_or_create(
+                            icao=row[0],
+                            number=row[1],
+                            di=row[2],
+                            line=line,
+                            departure=airport_departure,
+                            arrival=airport_arrival,
+                            departure_date=row[6],
+                            departure_date_real=row[7],
+                            arrival_date=row[8],
+                            arrival_date_real=row[9],
+                            status=flight_status,
+                            code=code
+                        )
+
+                    except Airport.DoesNotExist:
+                        print('Aeroporto não existe!')
+
+        except FileNotFoundError:
+            print('Dados do mês {month} de {year} não foram encontrados.'.format(
+                month=month, year=year))
